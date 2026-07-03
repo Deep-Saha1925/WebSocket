acf5537f67d38a1764fab50c0a3eed3c8f6cf0c3
Deep-Saha1925 <dip23447@gmail.com>
2026-07-03 22:21:25 +0530

    Serving html file

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import http from 'node:http'
 import {WebSocketServer} from 'ws';
+import fs from 'node:fs/promises';
+import path from 'path';
 
 const PORT = process.env.PORT ?? 8000;
 
-const httpServer = http.createServer(async function (req, res) {} );
+const httpServer = http.createServer(async function (req, res) {
+    const indexFile = fs.readFile(path.resolve('./index.html'), 'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(indexFile);
+});
 
 const wsServer = new WebSocketServer({server: httpServer});
+
+wsServer.on('connection', (websocket) => {
+    console.log(`Websocket Connection...`);
+    websocket.on('message', (message) => {
+        console.log(message.toString());
+    })
+})
+
 
 httpServer.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
