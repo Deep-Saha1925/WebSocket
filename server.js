46b1cf2ca01dbe2e0ef241056bba479546a4874b
Deep-Saha1925 <dip23447@gmail.com>
2026-07-03 22:05:16 +0530

    Websocker Server using HTTP

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import http from 'node:http'
+import {WebSocketServer} from 'ws';
 
 const PORT = process.env.PORT ?? 8000;
 
-const httpServer = http.createServer(async function (req, res) {} )
+const httpServer = http.createServer(async function (req, res) {} );
+
+const wsServer = new WebSocketServer({server: httpServer});
 
 httpServer.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
-})
+});
